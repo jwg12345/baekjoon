e16aecca1d5584335368df0a5c12853839a69f90
jwg12345 <154450394+jwg12345@users.noreply.github.com>
2024-01-25 16:12:52 +0900

    Add files via upload
    baekjoon11055

---
+++ bj11055.py
@@ -0,0 +1,13 @@
+n = int(input())
+A = list(map(int, input().split()))
+
+dp = [1] * n
+dp[0] = A[0]
+for i in range(1, n):
+    for j in range(i):
+        if A[i] > A[j]:
+            dp[i] = max(dp[i], dp[j] + A[i])
+        else:
+            dp[i] = max(dp[i], A[i])
+
+print(max(dp))
