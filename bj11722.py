d2a4cc5c22895e22f6e7de475e8373ae35d1f960
jwg12345 <154450394+jwg12345@users.noreply.github.com>
2024-01-26 13:05:40 +0900

    Add files via upload
    baekjoon11722

---
+++ bj11722.py
@@ -0,0 +1,12 @@
+n = int(input())
+A = list(map(int, input().split()))
+
+dp = [1] * n
+for i in range(n):
+    for j in range(i): # i 이전 값
+        if A[j] > A[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(max(dp))
+     
+
