4ed2a49a1a6577c77d2d79ceb8662d8128da7707
jwg12345 <154450394+jwg12345@users.noreply.github.com>
2024-02-06 04:46:43 +0900

    Add files via upload
    baekjoon15649

---
+++ bj15649.py
@@ -0,0 +1,16 @@
+n, m = map(int, input().split())
+
+p = []
+
+def dfs():
+    if len(p) == m:
+        print(" ".join(map(str, p)))
+        return
+
+    for i in range(1, n + 1):
+        if i not in p:
+            p.append(i)
+            dfs()
+            p.pop()
+
+dfs()        
