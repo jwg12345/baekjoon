8c73509fa3936004980f244ac7f6370bf4c2a106
jwg12345 <154450394+jwg12345@users.noreply.github.com>
2024-02-19 16:32:26 +0900

    Add files via upload
    baekjoon10972

---
+++ bj10972.py
@@ -0,0 +1,14 @@
+n = int(input())
+p = list(map(int, input().split()))
+
+for i in range(n-1, 0, -1):
+    if p[i-1] < p[i]:
+        for j in range(n-1, 0, -1):
+            if p[i-1] < p[j]:
+                p[i-1], p[j] = p[j], p[i-1]
+                p = p[:i] + sorted(p[i:])
+                print(*p)
+                exit()
+
+print(-1)
+
